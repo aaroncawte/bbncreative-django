dcc2a25337e3b6f2c5fa85f8487f17bb07fbd6bf
Aaron Cawte <aaron@bbncreative.co>
2018-06-10 16:31:02 +0100

    Sensitive Information Moved To secrets.py

---
+++ bbncreative/secrets_template.py
@@ -0,0 +1,12 @@
+# This file emulated the "secrets.py" file used to keep sensitive data relevant to the web server.
+# It exists to show its functionality publicly, while the production file is concealed for obvious reasons.
+
+DJANGO_SECRET = ""
+
+POST_DB = {
+    "NAME": "",
+    "USER": "",
+    "PASSWORD": "",
+    "HOST": "",
+    "PORT": "",
+}
