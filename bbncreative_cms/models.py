208a4901aae739add186988a3bf4a586f8b46283
Aaron Cawte <aaron@bbncreative.co>
2018-06-17 22:10:56 +0100

    Started Database Models

--- bbncreative_cms/models.py
+++ bbncreative_cms/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(
+        default="New Project",
+        max_length=255
+    )
+
+
+class Collaborator(models.Model):
+    # Collaborator's name e.g. "Aaron Cawte"
+    name = models.CharField(
+        default="Collaborator Name",
+        max_length=255
+    )
+    # Optional collaborator URLs
+    url = models.URLField()
+    twitter = models.CharField(
+        default="",
+        max_length=15
+    )
+
+
+class Credit(models.Model):
+    # A credit credits a collaborator for something
+    collaborator = models.ForeignKey(
+        Collaborator,
+        on_delete=models.PROTECT
+    )
+    # A credit is specific to a project
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+    # A credit credits a collaborator for some action
+    action = models.CharField(
+        default="",
+        max_length=255
+    )
