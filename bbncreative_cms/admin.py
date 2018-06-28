ce785dd58137f924b185880fc07d21cbc37ec84a
Aaron Cawte <aaron@bbncreative.co>
2018-06-28 20:43:18 +0100

    All Sorts of Progress

--- bbncreative_cms/admin.py
+++ bbncreative_cms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Project, Collaborator, Credit, ImageAsset, EmbeddedAsset
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Collaborator)
+admin.site.register(Credit)
+admin.site.register(ImageAsset)
+admin.site.register(EmbeddedAsset)
--- bbncreative_cms/urls.py
+++ bbncreative_cms/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('projects/', views.projects, name="projects"),
+
+    path('projects/<slug:url_name>', views.project_from_name, name="project_by_name"),
+
+    path('assets/<slug:url_uuid>/<str:file_name>', views.image_asset, name="image_asset")
 ]
