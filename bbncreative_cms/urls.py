35083edb635105d7b2d3b4e4f2b1de404cceb8db
Aaron Cawte <aaron@bbncreative.co>
2018-07-13 21:19:36 +0100

    HTML & CSS Work

--- bbncreative_cms/urls.py
+++ bbncreative_cms/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('projects/<slug:url_name>', views.project_from_name, name="project_by_name"),
     path('feeds/<slug:url_name>', views.feed_from_name, name="feed_by_name"),
 
-    path('assets/<slug:url_uuid>/<str:file_name>', views.image_asset, name="image_asset")
+    path('assets/<slug:url_uuid>/<str:file_name>', views.image_asset, name="image_asset"),
+
+    path('testpage', views.test_page, name="test_page")
 ]
