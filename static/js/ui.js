804f11ab71b3b70be69a071c3e5575a55ccc22a2
Aaron Cawte <aaron@bbncreative.co>
2018-07-19 13:42:49 +0100

    Menu Buttons (Almost) Work!

--- static/js/ui.js
+++ static/js/ui.js
@@ -11,9 +11,48 @@ $(function () {
     let windowHeight = window.innerHeight;
 
     let firstWindowHeight = absoluteMinimum;
-    $('.window-first').css("min-height", stringifyPixel(firstWindowHeight));
+    $(".window-first").css("min-height", stringifyPixel(firstWindowHeight));
 
     let heroWindowHeight = Math.max(windowHeight - offset, absoluteMinimum);
-    $('.window-hero').css("min-height", stringifyPixel(heroWindowHeight));
+    $(".window-hero").css("min-height", stringifyPixel(heroWindowHeight));
 
+    /* Menu Display Mechanics
+   ======================================================================== */
+    const $menuGlobal = $("#menuGlobal");
+    const $menuProjects = $("#menuProjects");
+    const $menuFeeds = $("#menuFeeds");
+    const $allMenus = $("#menuGlobal, #menuProjects, #menuFeeds");
+
+    const $closeMenuButton = $('.menu-close');
+
+    const $mbGlobal = $("#menuGlobalButton");
+    const $mbProjects = $("#menuProjectsButton, #inMenuProjectsButton");
+    const $mbFeeds = $("#menuFeedsButton, #inMenuFeedsButton");
+
+    function showOrHide(menu) {
+        if (menu.hasClass("menu-hidden")) {
+            $allMenus.addClass("menu-hidden");
+            menu.removeClass("menu-hidden");
+        } else {
+            menu.addClass("menu-hidden");
+        }
+    }
+
+    $mbGlobal.on("click", function () {
+        showOrHide($menuGlobal);
+    });
+    $mbProjects.on("click", function () {
+        showOrHide($menuProjects);
+    });
+    $mbFeeds.on("click", function () {
+        showOrHide($menuFeeds);
+    });
+
+    $closeMenuButton.on('click', function () {
+        $allMenus.addClass("menu-hidden");
+    });
+
+    $('.window').on('click', function () {
+        $allMenus.addClass("menu-hidden");
+    });
 });
