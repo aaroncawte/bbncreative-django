fecfd01f7c0037d5aefd65d62227cc5c1c1d9ebc
Aaron Cawte <aaron@bbncreative.co>
2018-07-21 14:38:28 +0100

    Context Processors for top Feeds & Projects

---
+++ bbncreative_cms/context_processors.py
@@ -0,0 +1,14 @@
+from .models import Project, Feed
+
+NUM_TOP_PROJECTS = 3
+NUM_TOP_FEEDS = 3
+
+
+def top_projects(context):
+    projects = Project.objects.order_by('-date_complete')[:NUM_TOP_PROJECTS]
+    return {'top_projects': projects}
+
+
+def top_feeds(context):
+    feeds = Feed.objects.all()[:NUM_TOP_FEEDS]
+    return {'top_feeds': feeds}
