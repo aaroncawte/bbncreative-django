31767f90fe574a681dc2023c41b5843c74c16fd9
Aaron Cawte <aaron@bbncreative.co>
2018-07-21 14:38:28 +0100

    Context Processors for top Feeds & Projects

--- bbncreative/settings.py
+++ bbncreative/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bbncreative_cms.context_processors.top_projects',
+                'bbncreative_cms.context_processors.top_feeds'
             ],
         },
     },
