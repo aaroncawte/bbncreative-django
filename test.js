5f255946a13e88ce12a6ed7947d7a89650db0971
Aaron Cawte <aaron@bbncreative.co>
2018-08-06 15:45:58 +0100

    No Active JS Tests

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 import test from 'ava';
 
+console.log("Testing from test.js");
+
 test('foo', t => {
     t.pass();
-});
-
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-
-    t.is(await bar, 'bar');
 });
